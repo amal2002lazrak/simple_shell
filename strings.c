139bfc2af187166c87032d14c0d0ff7dc25b565d
amal2002lazrak <amallazrak221@gmail.com>
2023-11-11 23:49:43 +0100

    updated

---
+++ strings.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ *_strdup - duplicates a string
+ *@str: string to duplicate
+ *
+ *Return: pointer to duplicated stringvor NULL if it fails
+*/
+
+char *_strdup(char *str)
+{
+	char *strdp;
+	unsigned int i, j;
+
+	if (str == 0)
+	return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{}
+	strdp = (char *) malloc(sizeof(char) * (i + 1));
+
+	if (strdp == 0)
+	return (NULL);
+
+	for (j = 0; j <= i; j++)
+	strdp[j] = str[j];
+
+	return (strdp);
+
+}
+
+/**
+ *_strlen - returns the length of a string
+ *@s: the string input
+ *
+ *Return: the lenght
+*/
+
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
